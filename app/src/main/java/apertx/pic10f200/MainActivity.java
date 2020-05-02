e28854ae2e590569c0f683b47012fdff244b10d1
Apertx <Dirty0wolf@gmail.com>
2020-05-02 09:58:18 +0300

    Initial commit

---
+++ app/src/main/java/apertx/pic10f200/MainActivity.java
@@ -0,0 +1,14 @@
+package apertx.pic10f200;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
